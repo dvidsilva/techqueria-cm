768b7e8533779c609535eb43d49052c345d0596b
david <contacto@dvidsilva.com>
2021-03-03 22:09:25 -0500

    added link to discord

--- pages/index.js
+++ pages/index.js
@@ -5,59 +5,60 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Techqueria Community</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <nav>
+        <div className="container mx-auto px-6 py-2 flex ">
+          <a className="font-bold text-2xl lg:text-4xl" href="#">
+            Techqueria &nbsp;
+            <img src="/favicon.ico" alt="" className="inline-block h-8	" />
+          </a>
+          {/* <div class="block lg:hidden">
+            <button class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-800 hover:border-teal-500 appearance-none focus:outline-none">
+              <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <title>Menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            </button>
+          </div> */}
+          {/* <div class="hidden lg:block">
+            <ul class="inline-flex">
+              <li><a class="px-4 font-bold" href="/">Home</a></li>
+              <li><a class="px-4 hover:text-gray-800" href="#">About</a></li>
+              <li><a class="px-4 hover:text-gray-800" href="#">Contact</a></li>
+            </ul> */}
+          {/* </div> */}
+        </div>
+      </nav>
+
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Techqueria
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://discord.gg/qq8fYU2z3g" target="_blank" className={styles.card}>
+            <h3>Discord &rarr;</h3>
+            <p>Join us on discord for fun music and chats!</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="https://www.techqueriacommunitynetwork.com/" target="_blank" className={styles.card}>
+            <h3>Open letter to Techqueria</h3>
+            <p>Letter with some requests to Techqueria!</p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          {/* Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} /> */}
         </a>
       </footer>
     </div>
